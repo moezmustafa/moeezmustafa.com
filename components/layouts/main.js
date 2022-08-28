65dee6b342d8c37f24e8b94b1926e4902566e0d0
Moez <moeez.mustafa@outlook.com>
2022-08-28 22:46:58 +0200

    base and navebar done

---
+++ components/layouts/main.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import { Box } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
+ import Navbar from "../navbar"
+
+const Main = ({ children ,router }) => { 
+
+	return ( 
+		<Box as="main" pb ={8}>
+		<Head>
+		<meta name="viewport"  content="width=device-width , initial-scale=1 " />
+		<title> Moeez Mustafata -Homepage </title>
+		</Head>
+
+		<Navbar path ={router.asPath}/>
+
+	<Container maxW="container.md" pt={14}>
+		{children}
+	</Container>
+
+	</Box>
+)
+}
+
+export default Main 
+
+		
---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+
+const options = {
+	arrowsParens: 'avoid',
+	singleQuote: true ,
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	semi : false, 
+	tabWidth: 2,
+	trailingComma : 'none'
+	
+
+
+}
+
+module.exports = options 
